c4aeb476f9eb98d8c6aa4a680a872871a2926919
Ashutosh <ashutoshmedge9@gmail.com>
2025-03-18 12:28:46 +0530

    this is commit from Asian Guy

--- src/test/java/practiceagain/Array2.java
+++ src/test/java/practiceagain/Array2.java
@@ -4,11 +4,13 @@ public class Array2 {
 
 	public static void main(String[] args) {
 		
-		int a[] = new int[4];
+		int a[] = new int[6];
 		a[0] = 23;
 		a[1]= 78;
 		a[2]= 24;
 		a[3]= 25;
+		a[5]=5;
+		a[6]=6;
 		
 		System.out.println(a[5]);			
 		
